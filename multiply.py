78202ade1093391e0a3697c1a2981a1bad5c1846
Deepthi798 <deepthi.chowdary00@gmail.com>
2025-03-03 15:24:03 +0530

    Message

--- multiply.py
+++ multiply.py
@@ -1,6 +1,6 @@
 a=int(input('Enter a number :'))
 b=int(input('Enter a number :'))
-sum=a+b
-print('Addition of 2 numbers is :',sum)
-print('Difference of 2 numbers is :',a-b)
+multiply=a*b
+print('Multiplication of 2 numbers is :',multiply)
+print('Division of 2 numbers is :',a%b)
 print("OK")
